915344b8fa396bf6a2388f9d919763b7d4354966
Miguel Ángel Manzano <mznmiguel@gmail.com>
2023-03-09 19:32:01 +0100

    Fix SerializeToJSON method to get attribute values correctly.

--- src/n3exercici1/SerializeToJSON.java
+++ src/n3exercici1/SerializeToJSON.java
@@ -4,16 +4,41 @@ import java.lang.reflect.Field;
 
 
 public class SerializeToJSON {
+	/*
+	 * This method receives an object and serializes all its attributes to JSON.
+	 * */
 	public static <T> String serializeToJSON(T object) {
 		T o = object;
+		String field_name;
+		Field field;
 		StringBuilder sb = new StringBuilder();
+		String value;
 		
 		Class<?> aClass = o.getClass();
 		Field[] fields = aClass.getDeclaredFields();	
 		
 		sb.append("{");
 		for (int i = 0; i < fields.length; i++) {
-			sb.append("\"").append(fields[i].getName()).append("\":\"").append(fields[i]).append("\",");
+			try {
+				field_name = fields[i].getName();				// It gets the name of the attribute
+				field = aClass.getDeclaredField(field_name);    // It gets the field with specific name
+				field.setAccessible(true);    					// Makes non-public fields accessible to read their value
+				value = (String) field.get(o); 					// Reads the value of the field
+				if (i < fields.length - 1) {
+					sb.append("\"").append(field_name).append("\":\"").append(value).append("\",");
+				} else {  			// if the field is the last attribute it does not add a comma
+					sb.append("\"").append(field_name).append("\":\"").append(value).append("\"");
+				}
+				field.setAccessible(false);					    // make the field inaccessible again
+			} catch (NoSuchFieldException e) {
+				e.printStackTrace();
+			} catch (SecurityException e) {
+				e.printStackTrace();
+			} catch (IllegalArgumentException e) {
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+			}
 		}
 		sb.append("}");
 		
